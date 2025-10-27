4c670bd12be73e082b435d5445eaca6d44ccc3ec
Anton-Mosquito <anton0886@outlook.com>
2025-10-27 14:51:59 +0200

    Normalize line endings and add .gitattributes

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const hello = ref("Hello, Nuxt 4!");
+</script>
+
+<template>
+  <div>
+    {{ hello }}
+    <SharedUsersTestComponent />
+    <WidgetComponent />
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
