dda513c9d9fbf3e2fdc54e6454ec1b0ef110c4c8
Anton Komarnytskyi <anton0886@outlook.com>
2025-12-30 09:50:05 +0200

    feat(AddFavorite): define props for AddFavorite component

--- app/types/components/ui/button.ts
+++ app/types/components/ui/button.ts
@@ -19,3 +19,10 @@ export interface ButtonProps {
 export type ButtonEmits = {
   click: [];
 };
+
+
+export interface AddFavoriteProps {
+  id: number;
+  isShown?: boolean;
+  variant?: "card" | "icon";
+}
