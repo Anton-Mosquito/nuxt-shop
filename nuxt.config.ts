872585f1e4d0c65c9ad9d120fb9ebd55a298ae77
Anton Komarnytskyi <anton0886@outlook.com>
2026-01-12 20:34:01 +0200

    refactor(config): remove commented layoutTransition from nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -130,7 +130,6 @@ export default defineNuxtConfig({
       script: [],
     },
     pageTransition: { name: "page", mode: "out-in" },
-    // layoutTransition: { name: "layout", mode: "out-in" },
   },
   sitemap: {
     sources: ["~/server/api/sitemap/url.ts"],
